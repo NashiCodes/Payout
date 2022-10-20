52aebce3e77407003f36ced0319f39d777d1a7e1
João Pereira <nashi.codes@gmail.com>
2022-10-20 00:31:03 -0300

    implementado a homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,3 +1,16 @@
+import styles from "./Home.module.css";
+import saving from "../../img/saving.svg";
+import LinkButton from "../layout/LinkButton";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className={styles.home_container}>
+      <h1>
+        Bem-Vindo ao <span>Payout</span>
+      </h1>
+      <p>Comece a gerenciar os seus prrojetos agora mesmo!</p>
+      <LinkButton to="/newproject" text="Criar Novo Projeto" />
+      <img src={saving} alt="Poupança" />
+    </section>
+  );
 }
