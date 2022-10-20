34c9c9a78f247373ca516a9311e184c9e31d1935
João Pereira <nashi.codes@gmail.com>
2022-10-20 00:48:37 -0300

    esqueleto do formulario

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,3 +1,12 @@
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
+
 export default function NewProject() {
-  return <h1>NewProject</h1>;
+  return (
+    <div className={styles.NP_container}>
+      <h1>Criar Projetos</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm />
+    </div>
+  );
 }
