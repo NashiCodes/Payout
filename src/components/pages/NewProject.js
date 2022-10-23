783ef3e872992f4d23e14167b5f7ea5d16a1d839
João Pereira <nashi.codes@gmail.com>
2022-10-23 16:51:08 -0300

    implementing a message alert

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -3,7 +3,7 @@ import ProjectForm from "../project/ProjectForm";
 import styles from "./NewProject.module.css";
 
 export default function NewProject() {
-  const history = useNavigate();
+  const navigate = useNavigate();
 
   function createPost(project) {
     //initialize cost and services
@@ -23,7 +23,7 @@ export default function NewProject() {
         console.log(data);
         console.log("====================================");
 
-        history("/projects", { message: "Projeto criado com sucesso!" });
+        navigate("/projects", { message: "Projeto criado com sucesso!" });
       })
       .catch((err) => console.log(err));
   }
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,3 +1,14 @@
+import { useLocation } from "react-router-dom";
+import Message from "../layout/Message";
+
 export default function Projects() {
-  return <h1>Projects</h1>;
+  const navigate = useLocation();
+  let message = "";
+  if (navigate.state) message = navigate.state.message;
+  return (
+    <div>
+      <h1>Meus Projetos</h1>
+      {message && <Message msg={message} type="success" />}
+    </div>
+  );
 }
